e91b7a28e061a37232ae0f85d2fcc374701a62dd
anelli <anuash.anelya07@gmail.com>
2025-06-04 15:39:40 +0500

    Initial Commit

---
+++ Todo App/Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_AppApp.swift
+//  Todo App
+//
+//  Created by Anel Anuash on 04.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
