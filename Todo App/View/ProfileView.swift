f98365fc4eb87947de4bedbea5375cfbe9862d93
anelli <anuash.anelya07@gmail.com>
2025-06-09 11:00:12 +0500

    updated ProfileView

--- Todo App/View/ProfileView.swift
+++ Todo App/View/ProfileView.swift
@@ -1,6 +1,9 @@
 import SwiftUI
 
+
 struct ProfileView: View {
+    @EnvironmentObject var authService: AuthService
+
     var body: some View {
         NavigationStack {
             VStack {
@@ -14,13 +17,12 @@ struct ProfileView: View {
                         .shadow(color: .blue.opacity(0.8), radius: 7, x: 0, y: 0)
                     Image("profile")
                         .resizable()
-                        .frame(width: 100, height: 140)
+                        .frame(width: 140, height: 140)
                         .clipShape(Circle())
                 }
                 
                 //Profile name
-                Text ("Anel Anuash")
-                    .font(.title)
+                Text("\(authService.user?.firstName ?? "") \(authService.user?.lastName ?? "")")                    .font(.title)
                     .bold()
                 
                     .padding(0.5)
